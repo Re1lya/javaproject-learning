5a8c0689b2f9ab3fc8dd6e33ab84a05086c86ebe
温家正 <2097382371@qq.com>
2026-04-06 14:59:16 +0800

    工作台业务模块功能代码

--- sky-server/src/main/java/com/sky/controller/admin/ReportController.java
+++ sky-server/src/main/java/com/sky/controller/admin/ReportController.java
@@ -3,6 +3,7 @@ package com.sky.controller.admin;
 import com.sky.result.Result;
 import com.sky.service.ReportService;
 import com.sky.vo.OrderReportVO;
+import com.sky.vo.SalesTop10ReportVO;
 import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import io.swagger.annotations.Api;
@@ -85,7 +86,7 @@ public class ReportController {
      */
     @GetMapping("/top10")
     @ApiOperation("销量排名top10")
-    public Result<OrderReportVO> top10(
+    public Result<SalesTop10ReportVO> top10(
             @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate begin,
             @DateTimeFormat(pattern = "yyyy-MM-dd") LocalDate end){
 
--- sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/ReportServiceImpl.java
@@ -11,6 +11,7 @@ import com.sky.vo.TurnoverReportVO;
 import com.sky.vo.UserReportVO;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
@@ -21,6 +22,7 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 
+@Service
 public class ReportServiceImpl implements ReportService {
 
     @Autowired
@@ -39,13 +41,11 @@ public class ReportServiceImpl implements ReportService {
     public TurnoverReportVO getTurnoverStatistics(LocalDate begin, LocalDate end) {
         //当前集合用于存放范围内的每天的日期
         List<LocalDate> dateList = new ArrayList<>();
-
         dateList.add(begin);
         while(!begin.equals(end)){
             begin=begin.plusDays(1);
             dateList.add(begin);
         }
-        dateList.add(end);
 
         //存放每天的营业额
         List<Double> turnoverList = new ArrayList<>();
@@ -82,15 +82,11 @@ public class ReportServiceImpl implements ReportService {
     public UserReportVO getUserStatistics(LocalDate begin, LocalDate end) {
         //存放从begin到end之间每天对应的日期
         List<LocalDate> dateList = new ArrayList<>();
-
-
         dateList.add(begin);
-
         while(!begin.equals(end)){
             begin=begin.plusDays(1);
             dateList.add(begin);
         }
-        dateList.add(end);
 
 
 
@@ -136,15 +132,11 @@ public class ReportServiceImpl implements ReportService {
     public OrderReportVO getOrderStatistics(LocalDate begin, LocalDate end) {
         //存放从begin到end之间每天对应的日期
         List<LocalDate> dateList = new ArrayList<>();
-
-
         dateList.add(begin);
-
         while(!begin.equals(end)){
             begin=begin.plusDays(1);
             dateList.add(begin);
         }
-        dateList.add(end);
 
         //存放订单总数
         List<Integer> orderCountList = new ArrayList<>();
